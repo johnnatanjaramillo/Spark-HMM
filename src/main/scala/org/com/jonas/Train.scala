9567c38e4f80ede1a052c10cb695d4b6278cea85
jonas <johnnatan999@gmail.com>
2018-04-19 22:43:21 -0500

    New parameter for train method

--- src/main/scala/org/com/jonas/Train.scala
+++ src/main/scala/org/com/jonas/Train.scala
@@ -8,10 +8,11 @@ object Train {
   /**
     * @param args
     * args(0): Path of sample
-    * args(1): Value of M
-    * args(2): Value of k
-    * args(3): Number of partitions (fw-bw process)
-    * args(4): Path to result
+    * args(1): Size of sample
+    * args(2): Value of M
+    * args(3): Value of k
+    * args(4): Number of partitions (fw-bw process)
+    * args(5): Path to result
     *
     * @return result
     * file with structure: M;k;finalPi,finalA,finalB
@@ -20,17 +21,17 @@ object Train {
     Logger.getLogger("org").setLevel(Level.ERROR)
     Logger.getLogger("akka").setLevel(Level.ERROR)
     val sparkSession = SparkSession.builder.appName("Spark-HMM").getOrCreate()
-    val sample: DataFrame = sparkSession.read.csv(args(0))
+    val sample: DataFrame = sparkSession.read.csv(args(0)).sample(withReplacement = false, args(1).toDouble)
       .withColumnRenamed("_c0", "workitem").withColumnRenamed("_c1", "str_obs")
     // BaumWelchAlgorithm(M, k, initialPi, initialA, initialB, numPartitions)
-    val result = hmm.BaumWelchAlgorithm.run(sample, args(1).toInt, args(2).toInt,
-      normalize(DenseVector.rand(args(1).toInt), 1.0),
-      hmm.Utils.mkstochastic(DenseMatrix.rand(args(1).toInt, args(1).toInt)),
-      hmm.Utils.mkstochastic(DenseMatrix.rand(args(1).toInt, args(2).toInt)),
-      args(3).toInt)
-    hmm.Utils.writeresult(args(4),
-      args(1) + ";" +
-        args(2) + ";" +
+    val result = hmm.BaumWelchAlgorithm.run(sample, args(2).toInt, args(3).toInt,
+      normalize(DenseVector.rand(args(2).toInt), 1.0),
+      hmm.Utils.mkstochastic(DenseMatrix.rand(args(2).toInt, args(2).toInt)),
+      hmm.Utils.mkstochastic(DenseMatrix.rand(args(2).toInt, args(3).toInt)),
+      args(4).toInt)
+    hmm.Utils.writeresult(args(5),
+      args(2) + ";" +
+        args(3) + ";" +
         result._1.toArray.mkString(",") + ";" +
         result._2.toArray.mkString(",") + ";" +
         result._3.toArray.mkString(","))
