eb2a49cc049a54825101d25645084aef146e2f0d
jonas <johnnatan999@gmail.com>
2018-04-21 08:04:15 -0500

    new Config Properties files

--- src/main/scala/org/com/jonas/Train.scala
+++ src/main/scala/org/com/jonas/Train.scala
@@ -1,5 +1,7 @@
 package org.com.jonas
 
+import java.io.FileInputStream
+
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import breeze.linalg.{DenseMatrix, DenseVector, normalize}
@@ -7,39 +9,45 @@ import breeze.linalg.{DenseMatrix, DenseVector, normalize}
 object Train {
   /**
     * @param args
-    * args(0): Generate logs
-    * args(1): Path of sample
-    * args(2): Size of sample
-    * args(3): Value of M
-    * args(4): Value of k
-    * args(5): Number of partitions (fw-bw process)
-    * args(6): Value of epsilon
-    * args(7): Max num iterations
-    * args(8): Path to result
-    *
+    * args(0): Config Properties File
     * @return result
-    * file with structure: M;k;finalPi,finalA,finalB
+    *         file with structure: M;k;finalPi,finalA,finalB
     */
   def main(args: Array[String]): Unit = {
-    if(args(0).equals("0")){
+    val applicationProps = new java.util.Properties()
+    val in = new FileInputStream(args(0))
+    applicationProps.load(in)
+    in.close()
+
+    if (applicationProps.getProperty("generate_logs").equals("true")) {
       Logger.getLogger("org").setLevel(Level.ERROR)
       Logger.getLogger("akka").setLevel(Level.ERROR)
     }
+
     val sparkSession = SparkSession.builder.appName("Spark-HMM").getOrCreate()
-    val sample: DataFrame = sparkSession.read.csv(args(1)).sample(withReplacement = false, args(2).toDouble)
+
+    val sample: DataFrame = sparkSession.read.csv(applicationProps.getProperty("path_sample"))
+      .sample(withReplacement = false, applicationProps.getProperty("size_sample").toDouble)
       .withColumnRenamed("_c0", "workitem").withColumnRenamed("_c1", "str_obs")
-    // BaumWelchAlgorithm(M, k, initialPi, initialA, initialB, numPartitions)
-    val result = hmm.BaumWelchAlgorithm.run(sample, args(3).toInt, args(4).toInt,
-      normalize(DenseVector.rand(args(3).toInt), 1.0),
-      hmm.Utils.mkstochastic(DenseMatrix.rand(args(3).toInt, args(3).toInt)),
-      hmm.Utils.mkstochastic(DenseMatrix.rand(args(3).toInt, args(4).toInt)),
-      args(5).toInt, args(6).toDouble, args(7).toInt)
-    hmm.Utils.writeresult(args(8),
-      args(3) + ";" +
-        args(4) + ";" +
+
+    // BaumWelchAlgorithm(observations, M, k, initialPi, initialA, initialB, numPartitions, epsilon, maxIterations)
+    val result = hmm.BaumWelchAlgorithm.run(sample,
+      applicationProps.getProperty("value_M").toInt,
+      applicationProps.getProperty("value_k").toInt,
+      normalize(DenseVector.rand(applicationProps.getProperty("value_M").toInt), 1.0),
+      hmm.Utils.mkstochastic(DenseMatrix.rand(applicationProps.getProperty("value_M").toInt, applicationProps.getProperty("value_M").toInt)),
+      hmm.Utils.mkstochastic(DenseMatrix.rand(applicationProps.getProperty("value_M").toInt, applicationProps.getProperty("value_k").toInt)),
+      applicationProps.getProperty("number_partitions").toInt,
+      applicationProps.getProperty("value_epsilon").toDouble,
+      applicationProps.getProperty("max_num_iterations").toInt)
+
+    hmm.Utils.writeresult(applicationProps.getProperty("path_result"),
+        applicationProps.getProperty("value_M") + ";" +
+        applicationProps.getProperty("value_k") + ";" +
         result._1.toArray.mkString(",") + ";" +
         result._2.toArray.mkString(",") + ";" +
         result._3.toArray.mkString(","))
     sparkSession.stop()
+
   }
 }
